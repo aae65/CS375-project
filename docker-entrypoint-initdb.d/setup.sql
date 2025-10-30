ff4ddb91aee8fabdf4c1ff95785afc00d5cf602b
viviantran210 <36091789+viviantran210@users.noreply.github.com>
2025-10-30 14:20:37 -0400

    testing docker compose for postgres img

--- docker-entrypoint-initdb.d/setup.sql
+++ docker-entrypoint-initdb.d/setup.sql
@@ -3,7 +3,7 @@
 -- use this to clear any existing tables to reinsert fresh data
 -- you'll need to add a DROP TABLE for every table you add
 -- we don't drop the database because that causes errors with fly
-DROP TABLE IF EXISTS foo;
+DROP TABLE IF EXISTS session;
 
 -- create whatever tables you need here
 CREATE TABLE session (
