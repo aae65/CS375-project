ec550d01b0740f1ee14888af38ef8eac0bb4a7c7
viviantran210 <36091789+viviantran210@users.noreply.github.com>
2025-10-30 14:08:23 -0400

    docker sql setup

---
+++ setup.sql
@@ -0,0 +1,19 @@
+\c restaurant-voting
+
+-- use this to clear any existing tables to reinsert fresh data
+-- you'll need to add a DROP TABLE for every table you add
+-- we don't drop the database because that causes errors with fly
+DROP TABLE IF EXISTS foo;
+
+-- create whatever tables you need here
+CREATE TABLE session (
+	id SERIAL PRIMARY KEY,
+	datum TEXT
+);
+
+-- dummy data
+INSERT INTO foo (datum) VALUES ('Hello this is some text');
+INSERT INTO foo (datum) VALUES ('Another sentence');
+INSERT INTO foo (datum) VALUES ('How are you?');
+
+\q
