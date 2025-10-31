b2a46bc546bad54f50943c2c05699d330e78b9c2
viviantran210 <viviantran210@gmail.com>
2025-10-31 17:33:35 -0400

    Co-authored-by: Legg2003 <Legg2003@users.noreply.github.com> created session related get requests that queries the Neon db to generate new session

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const { Pool } = require('pg');
+app.use(express.json());
 
 // connect to Neon db
 const pool = new Pool({
@@ -28,6 +29,40 @@ app.get("/api/test", async (req, res) => {
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
+});
+
+app.get("/generate-session", async (req, res) => {
+    let session_id = null;
+    pool.query(`INSERT INTO session 
+        DEFAULT VALUES RETURNING session_id;`)
+    .then((result) => {
+        let data = result.rows[result.rows.length - 1].session_id;
+        return res.json({
+            session_id: data,
+            link: `${req.protocol}://${req.get('host')}/session/${data}`
+        });
+    })
+    .catch((error) => {
+        console.error("Error generating a session:", error);
+        return res.status(500).json({ success: false, message: "Error generating a session." });
+    })
+});
+
+app.get("/session/:session_id", (req, res) => {
+    let session_id = req.params.session_id;
+
+    pool.query(`SELECT * FROM session WHERE session_id = $1`, [session_id])
+    .then((result) => {
+        if (result.rows.length === 0) {
+            return res.status(404).send("Session not found.");
+        } else {
+            return res.sendFile(__dirname + "/public/session.html");
+        }
+    })
+    .catch((error) => {
+        console.error("Error fetching session:", error);
+        return res.status(500).send("Error fetching session.");
+    });
 });
 
 app.listen(3000, "0.0.0.0", () => {
