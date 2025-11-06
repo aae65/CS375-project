c2f002248b105550370f29357ca908bacaf5f9a1
viviantran210 <36091789+viviantran210@users.noreply.github.com>
2025-11-06 12:45:32 -0500

    added to db schema

--- session.sql
+++ session.sql
@@ -1,3 +1,17 @@
+CREATE TABLE session (
+    session_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE session_settings (
+    session_id UUID REFERENCES session(session_id),
+    creater_user_id INT SERIAL,
+    username VARCHAR(50),
+    email VARCHAR(100),
+    zipcode VARCHAR(10),
+    end_date TIMESTAMP,
+);
+
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(50)
